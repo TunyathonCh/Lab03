f91a9302194b1f91adf17e12c1acbceb98dfb850
Tunyathon Chitmongkolthong <tunyathon.ch@mail.kmutt.ac.th>
2021-02-15 22:39:37 +0700

    lab03

---
+++ character.js
@@ -0,0 +1,6 @@
+        let namee = prompt('What is your favorite character?');
+        let color = prompt('what is the main color of '+namee+'?');
+        console.log(namee +' is your favorite character');
+        console.log(namee +' is '+color);
+        
+        
